b6381ada4cd08a2cd4d34703fcefa66f4dae42c6
Franco <francogutierrez04@gmail.com>
2023-12-04 01:18:21 -0300

    Solved challenge

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const container = document.querySelector('#container');
 
-for (i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    for (j = 0; j < 16; j++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
+let gridSize = 16;
+let interactions = 0;
+
+function setSize() {
+    let userInput = prompt('Enter the grid size: (between 16 and 100)');
+    while (userInput < 16 || userInput > 100 || userInput === null) {
+        if (userInput === null) {
+            return;
+        }
+        userInput = prompt('Please enter a size between 16 and 100:');
     }
-    container.appendChild(row);
+    gridSize = userInput;
+    container.innerHTML = '';
+    addGrid();
 }
+
+function addGrid() {
+    for (i = 0; i < gridSize; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (j = 0; j < gridSize; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = 40 / gridSize + 'em';
+            square.style.height = 40 / gridSize + 'em';
+
+            
+            square.addEventListener('mouseover', function() {
+                interactions++;
+                const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+
+                square.style.backgroundColor = randomColor;
+                square.classList.remove('transition-delay');
+            });
+            
+            square.addEventListener('mouseout', function() {
+                square.classList.add('transition-delay');
+                setTimeout(function() {
+                    square.style.backgroundColor = 'white';
+                }, 1000);
+            });
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+}
+
+addGrid();
